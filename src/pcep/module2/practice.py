33991bb458fdc038ddd42253b856338f4cf39dd8
Anand Sogalad <anand.sogalad305@gmail.com>
2025-09-28 17:26:42 +0530

    Implement Person class with weight validation and operator overloading, including unit tests for initialization, setter validation, and addition

---
+++ src/pcep/module2/practice.py
@@ -0,0 +1,21 @@
+# Practice: CLI apps for string/number manipulations, operator overloading
+
+
+class Person:
+    def __init__(self, name, age, weight):
+        self.__name = name
+        self.__age = age
+        self.weight = weight
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    @weight.setter
+    def weight(self, weight):
+        if not (1 <= weight <= 100):
+            raise ValueError("Person weight can be only within 1, 100")
+        self.__weight = weight
+
+    def __add__(self, other):
+        return self.weight + other.weight
