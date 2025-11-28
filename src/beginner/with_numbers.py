605e39265b747fd1b530d083bb2f06002d68c9cb
Anand Sogalad <anand.sogalad305@gmail.com>
2025-11-28 10:37:35 +0530

    basic arthmetic operations completed

---
+++ src/beginner/with_numbers.py
@@ -0,0 +1,44 @@
+# int is a whole number without any fraction part
+# float is number with fraction part
+
+
+# Arithmetic operators
+# Addition
+# Subtraction
+# Multiplication
+# Division
+# Floor Division
+# Exponention
+# Modulus
+
+if __name__ == "__main__":
+    num1 = 10
+    num2 = 5
+
+    # addtion
+    num3 = num1 + num2
+    print(f"Addition: {num3}")
+
+    # subtaction
+    num3 = num1 - num2
+    print(f"Subtraction: {num3}")
+
+    # multiplication
+    num3 = num1 * num2
+    print(f"Multiplication: {num3}")
+
+    # division
+    num3 = num1 / num2
+    print(f"Division: {num3}")
+
+    # floor division
+    num3 = num1 // num2
+    print(f"Floor Division: {num3}")
+
+    # exponention
+    num3 = num1**num2
+    print(f"Exponention: {num3}")
+
+    # modulus
+    num3 = num1 % num2
+    print(f"Modulus: {num3}")
